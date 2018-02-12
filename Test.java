3a433e971684a2ed6f01a7e95f8bc9659f586cf4
Anastasia Kravvari <akravvari@aueb.gr>
2018-02-12 19:53:09 +0200

    ekdosi 2 gia Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
 public class Test {
 
     public static void main (String args[]){
 
-        
+        Team celtics = new Team("Boston Celtics", 1946, 17);
+        Team bulls = new Team("Chicago Bulls", 1966, 6);
+        Team spurs = new Team("San Antonio Spurs", 1967, 5);
+        Team lakers = new Team("Los Angeles Lakers", 1947, 16);
+        Team bucks = new Team("Milwaukee Bucks", 1968, 1);
 
-    }
+        new Player ("Kyrie", "Irving", celtics, "American", 1.91, 88);
+        new Player ("Lauri", "Markkanen", bulls, "Finnish", 2.13, 104);
+        new Player ("Tony", "Parker", spurs, "French", 1.88, 84);
+        new Player ("Manu", "Ginobili", spurs, "Argentine", 1.98, 93);
+        new Player ("Luol", "Deng", lakers, "British", 2.06, 100);
+        new Player ("Giannis", "Antetokounmpo", bucks, "Greek", 2.11, 101); }
 }
